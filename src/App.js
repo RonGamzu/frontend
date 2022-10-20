6d27d03961946511781b9adbd1c4892614ece828
RonGamzu <ron3121998@gmail.com>
2022-10-21 00:57:43 +0300

    My front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import HotelsList from "./components/HotelsList";
+import { searchHotels } from "./DAL/api";
 
 function App() {
+  const [inputsData, setInputsData] = useState({});
+  const [hotelsToShow, setHotelsToShow] = useState([]);
+  const submitHandler = async (data) => {
+    if (!(data.ski_site && data.from_date && data.to_date && data.group_size)) {
+      return;
+    }
+    const hotelsList = await searchHotels(data);
+    setHotelsToShow(hotelsList);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        submitHandler={submitHandler}
+        inputsData={inputsData}
+        setInputsData={setInputsData}
+      />
+      <HotelsList inputsData={inputsData} hotelsToShow={hotelsToShow} />
+    </>
   );
 }
 
